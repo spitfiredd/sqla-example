5230086af5f17839c25ed1286baf2ffae6415405
Daniel Donovan <spitfiredd@gmail.com>
2019-02-11 07:23:32 -0500

    refactored trigger to func

--- examples/tsvector/create.py
+++ examples/tsvector/create.py
@@ -1,27 +1,10 @@
-from sqlalchemy import DDL, event
-
-from .model import Company
+from .model import Company, build_tsvector_trigger
 from ..common import create_table
 
 
 if __name__ == "__main__":
-    # use postgres built in trigger tsvector_update_trigger
-    # https://www.postgresql.org/docs/10/functions-textsearch.html
-    trig_searchable_tsvector_stmt = """
-    CREATE TRIGGER trig_searchable_tsvector BEFORE INSERT OR UPDATE
-    ON company
-    FOR EACH ROW EXECUTE PROCEDURE
-    tsvector_update_trigger(
-        searchable,'pg_catalog.english', 'name', 'about'
-    )
-    """
-    trig_searchable_tsvector = DDL(trig_searchable_tsvector_stmt)
 
-    event.listen(
-        Company.__table__,
-        'after_create',
-        trig_searchable_tsvector.execute_if(dialect='postgresql')
-    )
+    build_tsvector_trigger()
     # you have to create table after you create the triggers, I would do
     # some additional research about around this fact.
     create_table(Company)
--- examples/tsvector/model.py
+++ examples/tsvector/model.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, Text, Index, text
 from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.sql import func
+from sqlalchemy import DDL, event
+
 
 
 
@@ -34,4 +36,24 @@ class Company(SurrogatePK, Base):
             searchable,
             postgresql_using="gin"
         ),
+    )
+
+
+def build_tsvector_trigger():
+    # use postgres built in trigger tsvector_update_trigger
+    # https://www.postgresql.org/docs/10/functions-textsearch.html
+    trig_searchable_tsvector_stmt = """
+    CREATE TRIGGER trig_searchable_tsvector BEFORE INSERT OR UPDATE
+    ON company
+    FOR EACH ROW EXECUTE PROCEDURE
+    tsvector_update_trigger(
+        searchable,'pg_catalog.english', 'name', 'about'
+    )
+    """
+    trig_searchable_tsvector = DDL(trig_searchable_tsvector_stmt)
+
+    event.listen(
+        Company.__table__,
+        'after_create',
+        trig_searchable_tsvector.execute_if(dialect='postgresql')
     )
