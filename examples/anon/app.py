bf4f61c9d64f066a74bad5131060a2bd3538f38f
Daniel Donovan <spitfiredd@gmail.com>
2019-02-13 15:40:01 -0500

    refactor anon

--- examples/anon/app.py
+++ examples/anon/app.py
@@ -3,11 +3,10 @@ from faker import Faker
 from .anon import Anon
 from .users import User
 
-from ..common import session_factory
+from ..common import Database
 
 
-def populate_database():
-    session = session_factory()
+def populate_database(session):
     fake = Faker()
     for _ in range(5):
         user = User(name=fake.name(), ssn=fake.ssn())
@@ -18,4 +17,11 @@ def populate_database():
 
 
 if __name__ == "__main__":
-    populate_database()
+    db = Database(echo=True)
+    # drop tables if they both exists
+    if db.table_exists(Anon) and db.table_exists(User):
+        db.drop_table(User)
+        db.drop_table(Anon)
+    db.create_table(User)
+    db.create_table(Anon)
+    populate_database(db.session)
